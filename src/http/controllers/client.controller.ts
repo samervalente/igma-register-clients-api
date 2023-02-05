0d0cb4db8e7cda98460bf1755648434583a9008b
samervalente <samervalente@gmail.com>
2023-02-05 15:36:42 -0300

    feat: adjust client controller response and dto formats

--- src/http/controllers/client.controller.ts
+++ src/http/controllers/client.controller.ts
@@ -1,6 +1,15 @@
-import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpStatus,
+  Param,
+  Post,
+  Query,
+  Res,
+} from '@nestjs/common';
+import { Response } from 'express';
 import { CreateClientBody } from '../../dtos/client.dto';
-import { Client, ClientProps } from '../../entities/client.entity';
 import { ClientService } from '../../services/client.service';
 
 @Controller('/clients')
@@ -8,22 +17,32 @@ export class ClientController {
   constructor(private clientService: ClientService) {}
 
   @Post()
-  async create(@Body() body: CreateClientBody): Promise<void> {
-    await this.clientService.create(body);
+  async create(
+    @Body() body: CreateClientBody,
+    @Res() res: Response,
+  ): Promise<void> {
+    const client = await this.clientService.create(body);
+    res
+      .send({
+        response: 'Client registered sucessfully',
+        client,
+      })
+      .status(HttpStatus.CREATED);
   }
 
   @Get()
   async getAll(
     @Query('page') page = 1,
-    @Query('limit') limit = 10,
-  ): Promise<ClientProps[]> {
+    @Query('limit') limit = 40,
+    @Res() res: Response,
+  ) {
     const clients = await this.clientService.getAll(page, limit);
-    return clients;
+    res.send(clients).status(HttpStatus.OK);
   }
 
   @Get(':cpf')
-  async getByCPF(@Param() params: { cpf: string }): Promise<ClientProps> {
+  async getByCPF(@Param() params: { cpf: string }, @Res() res: Response) {
     const client = await this.clientService.getByCPF(params.cpf);
-    return client;
+    res.send(client).status(HttpStatus.OK);
   }
 }
