bf36c8d4b970d8afe463499ae96881725d699d86
lipivalente <lipivalente1@gmail.com>
2023-02-07 09:24:48 -0300

    Revert "chore: change optionally app_port env variable and seed birthDate data type"
    This reverts commit 5459b1ddff88b20071eee6cf547c2ae70b7c9139.

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,11 +1,10 @@
-import { faker } from '@faker-js/faker';
 import { PrismaClient } from '@prisma/client';
 import { validsCPF } from '../src/constants/client.constants';
 import { makeClient } from '../test/factories/client.factory';
 const prisma = new PrismaClient();
 async function main() {
   for (let i = 0; i < validsCPF.length; i++) {
-    const mockClient = makeClient({ cpf: validsCPF[i], birthDate: faker.date.birthdate() });
+    const mockClient = makeClient({ cpf: validsCPF[i] });
     await prisma.client.upsert({
       where: {
         cpf: mockClient.cpf,
