5459b1ddff88b20071eee6cf547c2ae70b7c9139
lipivalente <lipivalente1@gmail.com>
2023-02-07 09:09:55 -0300

    chore: change optionally app_port env variable and seed birthDate data type

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,10 +1,11 @@
+import { faker } from '@faker-js/faker';
 import { PrismaClient } from '@prisma/client';
 import { validsCPF } from '../src/constants/client.constants';
 import { makeClient } from '../test/factories/client.factory';
 const prisma = new PrismaClient();
 async function main() {
   for (let i = 0; i < validsCPF.length; i++) {
-    const mockClient = makeClient({ cpf: validsCPF[i] });
+    const mockClient = makeClient({ cpf: validsCPF[i], birthDate: faker.date.birthdate() });
     await prisma.client.upsert({
       where: {
         cpf: mockClient.cpf,
